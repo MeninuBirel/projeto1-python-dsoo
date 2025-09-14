3bc726b7bb5eed6a8f88118dc9255a0560b3b9ca
VictoriaHollerbach <hollerbach.victoria@gmail.com>
2025-09-14 15:49:38 -0300

    Create pessoa.py

---
+++ pessoa.py
@@ -0,0 +1,59 @@
+class Pessoa:
+
+    def __init__(self, nome: str, celular: str, identificacao: str, idade: int):
+        self.__nome = None
+        self.__celular = None
+        self.__identificacao = None
+        self.__idade = None
+        if isinstance(identificacao, str):
+            self.__identificacao = identificacao
+        if isinstance(nome, str):
+            self.__nome = nome
+        if isinstance(celular, str):
+            self.__celular = celular
+        if isinstance(idade, int):
+            if idade >= 18:
+                self.__idade = idade
+            else: 
+                print('Infelizmente menores de idade não podem participar da viagem')
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome: str):
+        if isinstance(nome, str):
+            self.__nome = nome
+
+
+    @property
+    def celular(self):
+        return self.__celular
+
+    @celular.setter
+    def celular(self, celular: str):
+        if isinstance(celular, str):
+            self.__celular = celular
+
+
+    @property
+    def identificacao(self):
+        return self.__identificacao
+
+    @identificacao.setter
+    def identificacao(self, identificacao: str):
+        if isinstance(identificacao, str):
+            self.__identificacao = identificacao
+    
+    
+    @property
+
+    @idade.setter
+
+
+    def valor_falta_pagar(self):
+    
+    def valor_pago(self):
+    
+    def verificar_se_pagou(self):

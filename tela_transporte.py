b134d92278082fe872479826aac6bdedd36b633b
Gabriel <gabrielgsouza4@gmail.com>
2025-09-27 17:02:57 -0300

    Create tela_transporte.py

---
+++ tela_transporte.py
@@ -0,0 +1,56 @@
+class TelaTransporte:
+    def menu_transporte(self):
+        print("\n----- Gestão de Transportes -----")
+        print("1. Cadastrar Transporte")
+        print("2. Listar Transportes")
+        print("0. Voltar ao Menu anterior")
+
+        while True:
+            try:
+                opcao = int(input("Escolha uma opção: "))
+                return opcao
+            except ValueError:
+                print("Opção inválida! Por favor, digite um número.")
+
+    def pega_dados_transporte(self, empresas: list) -> dict:
+        print("\n--- Cadastro de Novo Transporte ---")
+        print("Selecione a empresa:")
+        for i, empresa in enumerate(empresas):
+            print(f"{i + 1}. {empresa.nome}")
+
+        while True:
+            try:
+                escolha = int(input("Número da empresa: "))
+                if 1 <= escolha <= len(empresas):
+                    empresa_selecionada = empresas[escolha - 1]
+                    break
+                else:
+                    print("Número inválido.")
+            except ValueError:
+                print("Por favor, digite um número.")
+
+        tipo = input("Tipo do transporte: ")
+
+        while True:
+            try:
+                valor = float(input("Valor do transporte: R$ "))
+                break
+            except ValueError:
+                print("Valor inválido. Use ponto para decimais (ex: 150.50).")
+
+        return {
+            "empresa": empresa_selecionada,
+            "tipo": tipo,
+            "valor": valor
+        }
+
+    def mostra_transportes(self, transportes: list):
+        print("\n--- Lista de Transportes Cadastrados ---")
+        if not transportes:
+            print("Nenhum transporte cadastrado.")
+        else:
+            for i, transporte in enumerate(transportes, 1):
+                print(f"{i}. {transporte}")
+
+    def mostra_mensagem(self, msg: str):
+        print(f"\n>> {msg} <<")

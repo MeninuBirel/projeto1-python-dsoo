8bc6548872dbfc5d34d3a93c7dbb517d666a144b
VictoriaHollerbach <hollerbach.victoria@gmail.com>
2025-10-06 19:26:39 -0300

    Create pessoa.py

---
+++ entidade/pessoa.py
@@ -0,0 +1,51 @@
+class Pessoa:
+    def __init__(self, nome: str, celular: str, identificacao: str, idade: int):
+        self.__nome = None
+        self.__celular = None
+        self.__identificacao = None
+        self.__idade = None
+        if isinstance(identificacao, str):
+            self.__identificacao = identificacao
+        if isinstance(nome, str):
+            self.__nome = nome
+        if isinstance(celular, str):
+            self.__celular = celular
+        if isinstance(idade, int):
+            self.__idade = idade
+
+    @property
+    def nome(self):
+        return self.__nome
+    @nome.setter
+    def nome(self, nome: str):
+        if isinstance(nome, str):
+            self.__nome = nome
+            
+    @property
+    def celular(self):
+        return self.__celular
+    @celular.setter
+    def celular(self, celular: str):
+        if isinstance(celular, str):
+            self.__celular = celular
+            
+    @property
+    def identificacao(self):
+        return self.__identificacao
+    @identificacao.setter
+    def identificacao(self, identificacao: str):
+        if isinstance(identificacao, str):
+            self.__identificacao = identificacao
+
+
+    @property
+    def idade(self):
+        return self.__idade
+    @idade.setter
+    def idade(self, idade: int):
+        if isinstance(idade, int):
+            self.__idade = idade
+
+
+    def __str__(self):
+        return f'pessoa(nome={self.__nome}, celular={self.__celular}, identificacao={self.__identificacao}, idade={self.__idade})'

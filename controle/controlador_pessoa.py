cd07ba0a459d876f3e0bdf46c9a89f3c59cf7121
Gabriel <gabrielgsouza4@gmail.com>
2025-10-06 19:24:43 -0300

    Update and rename controlador_pessoa.py to controle/controlador_pessoa.py

--- controle/controlador_pessoa.py
+++ controle/controlador_pessoa.py
@@ -1,45 +1,50 @@
 from entidade.pessoa import Pessoa
 from limite.tela_pessoa import TelaPessoa
 
+
 class ControladorPessoas():
     def __init__(self, controlador_sistema):
         self.__tela_pessoa = TelaPessoa(self)
-        self._pessoas = []
+        self.__pessoas = []
         self.__controlador_sistema = controlador_sistema
-    
+
+    @property
+    def pessoas(self):
+        return self.__pessoas
+
     def find_pessoa_by_identificacao(self, identificacao: str):
         for pessoa in self.__pessoas:
             if pessoa.identificacao == identificacao:
                 return pessoa
         return None
-    
+
     def incluir_pessoa(self):
         dados_pessoa = self.__tela_pessoa.pega_dados_pessoa()
         identificacao = dados_pessoa['identificacao']
         pessoa = self.find_pessoa_by_identificacao(identificacao)
         if pessoa is None:
-            nova_pessoa = Pessoa(dados_pessoa['nome'], dados_pessoa['celular'], dados_pessoa['identificacao'], dados_pessoa['idade'])
+            nova_pessoa = Pessoa(dados_pessoa['nome'], dados_pessoa['celular'],
+                                 dados_pessoa['identificacao'], dados_pessoa['idade'])
             self.__pessoas.append(nova_pessoa)
             self.__tela_pessoa.mostra_mensagem("Pessoa cadastrada com sucesso!")
         else:
-            self.__tela_pessoa.mostra_mensagem("Erro: Essa pessoa já está cadstrada!")
-                return None
-          
+            self.__tela_pessoa.mostra_mensagem("Erro: Essa pessoa já está cadastrada!")
+        return None
+
     def excluir_pessoa(self):
         self.lista_pessoas()
-        identificacao = self.tela_pessoa.seleciona_pessoa()
+        identificacao = self.__tela_pessoa.seleciona_pessoa()
         pessoa_a_remover = self.find_pessoa_by_identificacao(identificacao)
-        if pessoa is not None:
+        if pessoa_a_remover is not None:
             self.__pessoas.remove(pessoa_a_remover)
-            self.tela_pessoa.mostra_mensagem('Pessoa removida com sucesso!')
+            self.__tela_pessoa.mostra_mensagem('Pessoa removida com sucesso!')
         else:
-            self.tela_pessoa.mostra_mensagem(f'Erro: essa pessoa NÃO está cadastrada!')
-          
+            self.__tela_pessoa.mostra_mensagem(f'Erro: essa pessoa NÃO está cadastrada!')
+
     def alterar_pessoa(self):
-        self.__lista_pessoas()
+        self.lista_pessoas()
         identificacao_pessoa = self.__tela_pessoa.seleciona_pessoa()
         pessoa = self.find_pessoa_by_identificacao(identificacao_pessoa)
-
         if pessoa is not None:
             novos_dados = self.__tela_pessoa.pega_dados_pessoa()
             pessoa.nome = novos_dados['nome']
@@ -52,21 +57,33 @@ class ControladorPessoas():
 
     def lista_pessoas(self):
         for pessoa in self.__pessoas:
-            self.__tela_pessoa.mostra_pessoa({'nome': pessoa.nome, 'celular': pessoa.celular, 'identificacao': pessoa.identificacao, 'idade': idade})
-    
-    def valor_falta_pagar(self):
-    
-    def valor_pago(self):
-    
-    def verificar_se_pagou(self):
-    
+            self.__tela_pessoa.mostra_pessoa({
+                'nome': pessoa.nome,
+                'celular': pessoa.celular,
+                'identificacao': pessoa.identificacao,
+                'idade': pessoa.idade
+            })
+
     def retornar(self):
         self.__controlador_sistema.abre_tela()
 
     def abre_tela(self):
-        lista_opcoes = {0: self.retornar, 1: self.inclui_pessoa, 2: self.exclui_pessoa,3: self.valor_falta_pagar, 4: self.valor_pago, 5: self.verificar_se_pagou}
+        lista_opcoes = {
+            0: self.retornar,
+            1: self.incluir_pessoa,
+            2: self.excluir_pessoa,
+            3: self.alterar_pessoa,
+            4: self.listar_pessoas
+        }
         while True:
             opcao = self.__tela_pessoa.mostra_tela_opcoes()
-            funcao_escolhida = lista_opcoes(opcao)
+            funcao_escolhida = lista_opcoes[opcao]
             funcao_escolhida()
-    
+
+    def listar_pessoas(self):
+        self.__tela_pessoa.mostra_mensagem("--- Lista de Pessoas Registradas ---")
+        if not self.__pessoas:
+            self.__tela_pessoa.mostra_mensagem("Nenhuma pessoa registrada.")
+            return
+        for pessoa in self.__pessoas:
+            self.__tela_pessoa.mostra_pessoas(pessoa)

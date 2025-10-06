f00b884e439e59a6847239bb05c1bd915aad8e0e
VictoriaHollerbach <hollerbach.victoria@gmail.com>
2025-10-06 19:39:20 -0300

    Create pix.py

---
+++ entidade/pix.py
@@ -0,0 +1,25 @@
+from entidade.pagamento import Pagamento
+from entidade.pessoa import Pessoa
+class Pix(Pagamento):
+    def __init__(self, valor: float, pessoa: Pessoa, data: str, cpf: str):
+        super().__init__(valor, pessoa, data)
+        self.__cpf = None
+        if isinstance(cpf, str):
+            self.__cpf = cpf
+ 
+    @property
+    def cpf(self):
+        return self.__cpf
+        
+    @cpf.setter
+    def cpf(self, cpf: str):
+        if isinstance(cpf, str):
+            self.__cpf = cpf
+        else:
+            raise ValueError("CPF deve ser uma string não vazia.")
+            
+    def get_metodo(self):
+        return "PIX"
+
+    def __str__(self):
+        return f"PIX - Valor: R$ {self.valor:.2f} | Pessoa: {self.pessoa.nome} | Data: {self.data} | CPF: {self.cpf}"

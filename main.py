e421f9ba4dcbc70d602b20f3b536ad39dda66d02
Gabriel <gabrielgsouza4@gmail.com>
2025-10-06 20:54:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-from controle.controlador_sistema_viagem import ControladorSistema
+from controle.controlador_sistema import ControladorSistema
 if __name__ == "__main__":
     ControladorSistema().inicializa_sistema()

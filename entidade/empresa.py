d6c286a7edfd4831524fc332458337d2986f33cf
VictoriaHollerbach <hollerbach.victoria@gmail.com>
2025-10-12 23:22:19 -0300

    Update empresa.py

--- entidade/empresa.py
+++ entidade/empresa.py
@@ -1,47 +1,45 @@
-class Empresa:
+class Empresa():
     def __init__(self, nome: str, cnpj: str, telefone: str):
         self.__nome = None
         if isinstance(nome, str):
             self.__nome = nome
-        self.__cnpj = self._formatar_cnpj(cnpj)
+        self.__cnpj = None
+        if isinstance(cnpj, str):
+            self.__cnpj = cnpj
         self.__telefone = None
         if isinstance(telefone, str):
             self.__telefone = telefone
-        self.transportes = []
+        self.__transportes = []
         
     @property
     def nome(self):
         return self.__nome
     
     @nome.setter
-    def nome(self, novo_nome: str):
-        if not isinstance(novo_nome, str) or not novo_nome.strip():
-            raise ValueError("Digite um nome válido")
-        self.__nome = novo_nome
+    def nome(self, nome: str):
+        if isinstance(nome, str):
+            self.__nome = nome
         
     @property
     def cnpj(self):
-        return (
-            f"{self.__cnpj[:2]}.{self.__cnpj[2:5]}.{self.__cnpj[5:8]}/"
-            f"{self.__cnpj[8:12]}-{self.__cnpj[12:]}"
-        )
+        return self.__cnpj
 
     @cnpj.setter
-    def cnpj(self, novo_cnpj: str):
-        self.__cnpj = self._formatar_cnpj(novo_cnpj)
+    def cnpj(self, cnpj: str):
+        if isinstance(cnpj, str):
+            self.__cnpj = cnpj
         
     @property
     def telefone(self):
         return self.__telefone
     
     @telefone.setter
-    def telefone(self, novo_telefone: str):
-        if not isinstance(novo_telefone, str) or not novo_telefone.strip():
-            raise ValueError("Digite um telefone válido")
-        self._telefone = novo_telefone
+    def telefone(self, telefone: str):
+        if isinstance(telefone, str):
+            self._telefone = telefone
         
     def adicionar_transporte(self, transporte):
-        self.transportes.append(transporte)
+        self.__transportes.append(transporte)
         
     def _formatar_cnpj(self, cnpj: str):
         if not isinstance(cnpj, str):

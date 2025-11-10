e3acb5639bfbdf5bb32a06fb29ab93cdcde06b9a
VictoriaHollerbach <hollerbach.victoria@gmail.com>
2025-11-10 11:00:02 -0300

    Update transporte.py

--- entidade/transporte.py
+++ entidade/transporte.py
@@ -1,13 +1,10 @@
 from entidade.empresa import Empresa
 
 class Transporte():
-    def __init__(self, tipo: str, empresa: Empresa):
+    def __init__(self, tipo: str):
         self.__tipo = None
         if isinstance(tipo, str):
             self.__tipo = tipo
-        self.__empresa = None
-        if isinstance(empresa, Empresa):
-            self.__empresa = empresa
             
     @property
     def tipo(self):
@@ -15,22 +12,9 @@ class Transporte():
     
     @tipo.setter
     def tipo(self, tipo: str):
-        if not isinstance(tipo, str) or not tipo.strip():
-            raise ValueError("O tipo de transporte deve ser um texto não vazio.")
-        self.__tipo = tipo
-        
-    @property
-    def empresa(self):
-        return self.__empresa
-    
-    @empresa.setter
-    def empresa(self, empresa: Empresa):
-        if not isinstance(empresa, Empresa):
-            raise TypeError("O atributo 'empresa' deve ser uma instância da classe Empresa.")
-        self.__empresa = empresa
+        if isinstance(tipo, str):
+            self.__tipo = tipo
         
     def __str__(self):
-        nome_empresa = self.empresa.nome if self.empresa else "N/A"
-        return (
-            f"Transporte - Tipo: {self.tipo}, Empresa: {nome_empresa}, "
-        )
+        return f'tipo: {self.__tipo}'
+        

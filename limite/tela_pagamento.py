62d366edbddb50054e2e60e7b53cb602fbe12cce
VictoriaHollerbach <hollerbach.victoria@gmail.com>
2025-11-10 11:05:45 -0300

    Update tela_pagamento.py

--- limite/tela_pagamento.py
+++ limite/tela_pagamento.py
@@ -14,7 +14,32 @@ class TelaPagamento():
                 print('Valor Incorreto: Digite um valor numérico inteiro válido')
                 if inteiros_validos:
                     print(f'Valores válidos: {inteiros_validos}')
+    
+    def seleciona_qual_viagem(self):
+        controlador_viagem = self.__controlador.controlador_viagem  #como fazer essa parte?????? sem dar tanta moral para a tela
+        viagens_cadastradas = controlador_viagem.viagens
+        print('\n--- Seleção da Viagem ---')
 
+        if not viagens_cadastradas:
+            self.mostra_mensagem("Erro: Não há viagens cadastradas.")
+            return None
+
+        opcoes_validas = []
+        for i, viagem in enumerate(viagens_cadastradas):
+            print(f"{i + 1} - Código: {viagem.codigo} | Nome: {viagem.nome} | Início: {viagem.data_inc} | Fim: {viagem.data_fim}")
+            opcoes_validas.append(i + 1)
+
+        opcoes_validas.append(0)  # Opção para Cancelar
+        while True:
+            escolha = self.le_num_inteiro(
+                "Selecione o número da viagem (0 para cancelar): ", opcoes_validas
+            )
+            if escolha == 0:
+                self.mostra_mensagem("Operação cancelada.")
+                return None
+            # Retorna o OBJETO viagem selecionado
+            return viagens_cadastradas[escolha - 1]
+    
     def mostra_tela_opcoes(self):
         print('-------GESTÃO DE PAGAMENTOS-------')
         print('1 - Registrar Novo Pagamento')
@@ -22,32 +47,27 @@ class TelaPagamento():
         print('0 - Retornar')
         opcao = self.le_num_inteiro('Escolha a opção: ', [0, 1, 2])
         return opcao
-
+    
     def seleciona_pessoa_pagamento(self, pessoas: list):
         print('\n--- Seleção do Pagador ---')
         if not pessoas:
             self.mostra_mensagem("Erro: Não há pessoas cadastradas para realizar o pagamento.")
             return None
-        
-        # Apresenta a lista de opções numeradas para o usuário fazer a escolha.
         for i, pessoa in enumerate(pessoas):
             print(f"{i+1} - Nome: {pessoa.nome} | ID: {pessoa.identificacao}")
 
-        # Garante que o usuário só saia do loop após digitar uma opção válida (um número).
         while True:
             try:
-                # Solicita a entrada do usuário e valida que a escolha é um número válido (ou zero).
                 opcoes_validas = [i + 1 for i in range(len(pessoas))] + [0]
                 escolha = self.le_num_inteiro("Selecione o número do pagador (0 para cancelar): ", opcoes_validas)
                 
                 if escolha == 0:
                     return None
                 
-                # Retorna o objeto Pessoa selecionado.
                 return pessoas[escolha - 1]
             except Exception:
                 self.mostra_mensagem("Opção inválida.")
-
+    
     def pega_dados_pagamento(self, pessoa_selecionada):
         print(f"\n----- Dados Pagamento para {pessoa_selecionada.nome} -----")
         data = input('Data (DD/MM/AAAA): ')
@@ -75,10 +95,10 @@ class TelaPagamento():
         # Se for PIX, adiciona a chave 'tipo' e coleta o CPF.
         if metodo == 1:
             dados['tipo'] = 'PIX'
-            dados['cpf'] = input('CPF: ')
+            dados['cpf'] = pessoa_selecionada.identificacao
         elif metodo == 2:
             dados['tipo'] = 'DINHEIRO'
-            dados['cpf'] = input('CPF: ')
+            dados['cpf'] = pessoa_selecionada.identificacao
         # Se for CARTÃO, adiciona o 'tipo' e coleta os dados específicos.
         elif metodo == 3:
             dados['tipo'] = 'CARTAO'

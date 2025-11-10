ca51a2843546461dcce097f1a54c0bb12173fbd0
VictoriaHollerbach <hollerbach.victoria@gmail.com>
2025-11-10 11:04:18 -0300

    Update tela_empresa.py

--- limite/tela_empresa.py
+++ limite/tela_empresa.py
@@ -14,6 +14,18 @@ class TelaEmpresa:
                 print('Valor Incorreto: Digite um valor numerico inteiro valido')
                 if inteiros_validos:
                     print(f'Valores válidos: {inteiros_validos}')
+    
+    def verificar_se_e_inteiro(self,  mensagem: str = 'Digite um valor: ', minimo: int = 0):
+        while True:
+            valor_lido = input(mensagem)
+            try:
+                valor = int(valor_lido)
+                if valor <= minimo:
+                    print(f'Número Incorreto: O valor deve ser maior que {minimo}.')
+                    continue
+                return valor
+            except ValueError:
+                print('Número Incorreto: Digite um valor numérico válido - números inteiros.')
 
     def mostra_tela_opcoes(self):
         print('-------MENU DE EMPRESAS-------')
@@ -28,22 +40,17 @@ class TelaEmpresa:
     def pega_dados_empresa(self):
         print('----- Dados Empresa -----')
         nome = input('Nome: ')
-        cnpj = input('CNPJ: ')
-        telefone = input('Telefone: ')
+        nome.capitalize()
+        cnpj = self.verificar_se_e_inteiro('cnpj: ', minimo=0)
+        telefone = self.verificar_se_e_inteiro('Telefone: ', minimo=0)
         if isinstance(nome, str) and isinstance(cnpj, str) and isinstance(telefone, str):
             return {'nome': nome, 'cnpj': cnpj, 'telefone': telefone}
-    
-    def mostra_empresa(self, dados_empresa):
-        print('Nome: ', dados_empresa['nome'])
-        print('Cnpj: ', dados_empresa['cnpj'])
-        print('Telefone: ', dados_empresa['telefone'])
-        print('\n')
 
     def mostra_empresas(self, empresa):
         self.mostra_mensagem(str(empresa))  # Usa o __str__ da Entidade
     
     def seleciona_empresa(self):
-        cnpj = input('CNPJ da empresa que deseja selecionar: ')
+        cnpj = self.verificar_se_e_inteiro('CNPJ da empresa que deseja selecionar: ', minimo=0)
         return cnpj
     
     def mostra_mensagem(self, mensagem):
